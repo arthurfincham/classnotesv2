c92445ff8759624b7e1cb4cd520fcf5ec84769e8
Arthur Fincham <arthurfincham@me.com>
2021-09-15 14:04:23 +0100

    refactor note model tagging

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,7 @@ class Note < ApplicationRecord
   validates :user_id, presence: true
   validates :note_date, presence: true
   belongs_to :user, optional: true
-  acts_as_taggable_on :tags, :instructor, :note_title
+  acts_as_taggable_on :tags
+  acts_as_taggable_on :instructors
+  acts_as_taggable_on :note_titles
 end
