a7608ff836b2a134e861f9f6aa27fbf4701c9e42
Arthur Fincham <arthurfincham@me.com>
2021-09-15 14:02:39 +0100

    add routes for tagged notes index

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,20 @@
 module NotesHelper
+
+  def format_date(note)
+    note.note_date.strftime("#{note.note_date.day.ordinalize} %B %Y")
+  end
+
+  def table_header
+    url = request.path_info
+    if url.include? 'tags'
+      request[:tag]
+    elsif url.include? 'instructors'
+      request[:instructor]
+    elsif url.include? 'note_titles'
+      request[:note_title]
+    else
+      "All Notes"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :notes, only: [:new, :create]
   root to: "notes#index"
+  get 'tags/:tag', to: 'notes#index', as: :tag
+  get 'instructors/:instructor', to: 'notes#index', as: :instructor
+  get 'note_titles/:note_title', to: 'notes#index', as: :note_title
 end
