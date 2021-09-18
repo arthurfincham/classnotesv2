9bce719c2baa8440d9adcef20421dbfad406b147
Arthur Fincham <arthurfincham@me.com>
2021-09-18 10:22:47 +0100

    reverted user model change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
-         skip_before_action :verify_authenticity_token
   validates_presence_of :username
   validates_uniqueness_of :username
   has_many :notes
