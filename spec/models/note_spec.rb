5f668718da07df917a69fc914d1e08a9dabf7b9b
Arthur Fincham <arthurfincham@me.com>
2021-09-18 09:07:59 +0100

    spec update

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe Note, type: :model do
     it { should validate_presence_of(:note_date) }
   end
 
-  it 'is valid with valid attributes' do
-    note = Note.new(user_id: 2, note_date: '2021-10-11')
-    expect(note).to be_valid
-  end
 
   it 'is not valid without a user_id' do
     note = Note.new(note_date: '2021-10-11')
