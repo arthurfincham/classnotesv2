78296af8cd11dfb40379f1d3b4c74d0f072a4087
Arthur Fincham <arthurfincham@me.com>
2021-09-18 10:21:43 +0100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
+         skip_before_action :verify_authenticity_token
   validates_presence_of :username
   validates_uniqueness_of :username
   has_many :notes
