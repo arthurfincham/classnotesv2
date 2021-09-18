0cdadbd08447738de792fd2b0478ff335ed33f1a
Arthur Fincham <arthurfincham@me.com>
2021-09-18 10:17:14 +0100

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
+         :recoverable, :rememberable, :validatable, :timeoutable
   validates_presence_of :username
   validates_uniqueness_of :username
   has_many :notes
