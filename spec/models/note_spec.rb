4b186aa477c152f1405e692ba7aeb564f0b38def
Arthur Fincham <arthurfincham@me.com>
2021-09-19 09:40:36 +0100

    spec for note model

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
 
-  describe 'validations' do
-    it { should validate_presence_of(:user_id) }
-    it { should validate_presence_of(:note_date) }
-  end
-
-
   it 'is not valid without a user_id' do
     note = Note.new(note_date: '2021-10-11')
     expect(note).to_not be_valid
@@ -18,6 +12,26 @@ RSpec.describe Note, type: :model do
     expect(note).to_not be_valid
   end
 
+  it 'is not valid without a title' do
+    note = Note.new(note_date: '2021-10-11', user_id: 2)
+    expect(note).to_not be_valid
+  end
+
+  it 'is valid with id, date and title of correct length' do
+    note = Note.new(note_date: '2021-10-11', user_id: 2, note_title: 'intermediate')
+    expect(note).to be_valid
+  end
+
+  it 'is not valid with a title > 14 chars' do
+    note = Note.new(note_date: '2021-10-11', user_id: 2, note_title: 'intermediate class')
+    expect(note).to_not be_valid
+  end
+
+  it 'accepts tags' do
+    note = Note.new(note_date: '2021-10-11', user_id: 2, note_title: 'intermediate')
+    note.tag_list.add("awesome, slick")
+    expect(note.tag_list).to eq ["awesome, slick"]
+  end
 end
 
 
