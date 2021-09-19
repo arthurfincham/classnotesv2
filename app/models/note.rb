e15cc12bc2622bb46453bfa69f5e9d1744ccbb59
Arthur Fincham <arthurfincham@me.com>
2021-09-19 09:40:30 +0100

    increase note_title permitted length

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :user_id, presence: true
   validates :note_date, presence: true
-  validates :note_title, presence: true, length: { minimum: 3, maximum: 12 }
+  validates :note_title, presence: true, length: { minimum: 3, maximum: 14 }
   acts_as_taggable
 end
